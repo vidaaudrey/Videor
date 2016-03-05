c210819166023c6475a0ec86bc707b479d210bb4
vidaaudrey <audrey@33road.com>
2016-03-05 15:59:06 -0800

    fix: add array empty check for youTubeUrlHelper

--- app/helpers/youtube-url-helper.js
+++ app/helpers/youtube-url-helper.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 
 export function youtubeUrlHelper(params/*, hash*/) {
-    console.log('video', params[0][0].key);
-  return `https://www.youtube.com/embed/${params[0][0].key}`;
+    if(params[0]){
+        console.log('video', params[0][0].key);
+
+      return `https://www.youtube.com/embed/${params[0][0].key}`;
+    }
 }
 
 export default Ember.Helper.helper(youtubeUrlHelper);
