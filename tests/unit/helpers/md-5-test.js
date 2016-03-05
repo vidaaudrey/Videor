6ff44e532347fba851fa603b21305ef976912f87
vidaaudrey <audrey@33road.com>
2016-03-05 09:01:38 -0800

    learn: add avatar-image component

---
+++ tests/unit/helpers/md-5-test.js
@@ -0,0 +1,10 @@
+import { md5 } from 'viedor/helpers/md-5';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | md 5');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = md5([42]);
+  assert.ok(result);
+});
