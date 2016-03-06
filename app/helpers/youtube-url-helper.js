19322b0a9bc3e35dd6d2d688f87c1663a38db86f
vidaaudrey <audrey@33road.com>
2016-03-05 21:19:54 -0800

    fix: fix youtube video display and add css flex-box to show movie list

--- app/helpers/youtube-url-helper.js
+++ app/helpers/youtube-url-helper.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 
 export function youtubeUrlHelper(params/*, hash*/) {
-    if(params[0]){
+    if(params[0] && params[0][0] && params[0][0].key){
         console.log('video', params[0][0].key);
-
       return `https://www.youtube.com/embed/${params[0][0].key}`;
-    }
+    } 
+    // if there is no trailer video, just return a funny cat video
+    return 'https://www.youtube.com/embed/tntOCGkgt98'
 }
 
 export default Ember.Helper.helper(youtubeUrlHelper);
